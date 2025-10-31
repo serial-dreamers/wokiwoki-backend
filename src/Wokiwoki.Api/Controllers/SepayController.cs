daa21d09888061610e07b4b02efb813ea87ad4c9
Flover Heon <hoangdnse182987@fpt.edu.vn>
2025-10-31 17:44:56 +0700

    GenerateQRCodeFromSepay_ConfirmBookingFromWebhook

---
+++ src/Wokiwoki.Api/Controllers/SepayController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Wokiwoki.Application.Features.Sepay.QRCode;
+
+namespace Wokiwoki.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SepayController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public SepayController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        ///<summary>
+        ///Generate QR Code for check out   
+        /// </summary>
+        public async Task<IActionResult> GenerateQRCode([FromBody] GenerateQRCodeCommand command)
+        {
+            var authHeader = Request.Headers["Authorization"].ToString();
+
+            // Gửi header xuống handler
+            var result = await _mediator.Send(command with { Authorization = authHeader });
+            return Ok(result);
+        }
+
+    }
+}
