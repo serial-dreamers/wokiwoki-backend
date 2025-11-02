00d7643930cbb6dab485a889ba40fcea8ccaf97e
HuynhNgocKhanh <huynhngockhanh1201@gmail.com>
2025-11-02 22:04:28 +0700

    add GetWorkshopsByOrganizationId

---
+++ src/Wokiwoki.Application/Features/Workshops/Queries/GetFilterPagedWorkshopsQuery/GetWorkshopsByOrganizationId.cs
@@ -0,0 +1,16 @@
+﻿using MediatR; 
+using Wokiwoki.Application.Common.Models;
+using Wokiwoki.Application.DTOs.Response;
+
+namespace Wokiwoki.Application.Features.Workshops.Queries.GetFilterPagedWorkshopsQuery
+{
+	public record GetWorkshopsByOrganizationIdQuery(
+		Guid organizationId,
+		int PageNumber = 1,
+		int PageSize = 10
+		) : IRequest<PaginatedList<WorkshopDto>>;
+
+	public class GetWorkshopsByOrganizationId
+	{
+	}
+}
